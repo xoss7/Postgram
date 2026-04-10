4742c8ff9822b58553d8b1d996b2e864e64ce059
Amath Thiam <codeforfun77@gmail.com>
2026-04-10 12:16:30 +0000

    :recycle: Refactoring

--- services/notification/src/main/java/sn/edu/ept/postgram/notificationservice/controller/NotificationController.java
+++ services/notification/src/main/java/sn/edu/ept/postgram/notificationservice/controller/NotificationController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/notifications")
+@RequestMapping("/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 
