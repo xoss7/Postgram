c3903ca052a955feb80990441d914f05bfa93e13
Amath Thiam <codeforfun77@gmail.com>
2026-04-21 00:58:03 +0000

    :rocket: Add a configuration server

---
+++ config-server/src/test/java/sn/edu/ept/postgram/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.ept.postgram.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
